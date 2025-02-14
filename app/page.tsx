e9c96442c203345d4dad9de3e33abd13f67a4034
GHULAM-1 <ilyasghulam35@gmail.com>
2025-02-15 02:49:30 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
-import Footer from "@/components/footer/footer";
-import SectionTwo from "@/components/section-2/section-2";
-import SectionThree from "@/components/section-3/section-3";
-import SectionFour from "@/components/section-4/section-4";
+import Hero from "@/components/header/hero";
+import Navbar from "@/components/header/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col gap-y-10">
-        <div>
-          <SectionTwo />
-        </div>
-        <div>
-          <SectionThree />
-        </div>
-        <div>
-          <SectionFour />
-        </div>
-      </div>
-      <Footer />
-    </main>
+    <div className="relative w-full">
+      <Hero />
+    </div>
   );
 }
